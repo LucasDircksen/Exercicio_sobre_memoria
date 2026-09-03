d5cb6481ea9c094244dc96ab3abcebfd9803a21e
Lucas Dircksen Balcevicz <26dirballucas@gmail.com>
2026-09-03 20:18:13 -0300

    Add files via upload

--- memoria.py
+++ memoria.py
@@ -1,12 +1,18 @@
 #2 - Memoria
 #Cada variável em Python é uma **referência** para um objeto na memória (heap). A lista não guarda os valores “dentro” dela de forma rígida: guarda ponteiros para os objetos.
 
+#EXEMPLO 1 --------------------------------------------------------------------------------------------------------------------
+
 a = [1, 2, 3]
 b = a          # b aponta para a MESMA lista
 b.append(4)
 print(a)       # [1, 2, 3, 4]
 
 c = a.copy()   # cópia rasa (nova lista, mesmos objetos)
+
+#------------------------------------------------------------------------------------------------------------------------------
+
+#EXEMPLO 2 --------------------------------------------------------------------------------------------------------------------
 
 x = [10] # define o valor de x como 10 
 y = [10] # define o valor de y como 10 porem em outro endereço de memória
@@ -24,3 +30,5 @@ if z == x:
     print("Iguais")
 else:
     print("Valores diferentes")
+
+#------------------------------------------------------------------------------------------------------------------------------
